a5523e1dd3f0de0503b4833fc7092a664e8f9ba0
limbang <495071565@qq.com>
2025-03-02 15:49:11 +0800

    feat: 定义一些错误提示产量

---
+++ src/main/kotlin/utils/MessageUtil.kt
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2025. limbang and contributors.
+ *
+ * 此源代码的使用受 GNU AGPLv3 许可证的约束，该许可证可在"LICENSE"文件中找到。
+ * Use of this source code is governed by the GNU AGPLv3 license that can be found in the "LICENSE" file.
+ */
+
+package top.limbang.remoteoc.utils
+
+const val TIMEOUT_ERROR = "❌ 等待结果超时"
+const val REQUEST_FAILED = "❌ 命令请求失败"
+const val RESULT_FAILED = "❌ 获取结果失败"
+
